ac3627bc8ad10a4d5dda204201d325a969f1bd5d
Gabriel Ramos <gabriel.luiz.ramos@gmail.com>
2019-02-01 11:31:13 -0200

    update brew script

--- scripts/apps/brew.sh
+++ scripts/apps/brew.sh
@@ -1,18 +1,44 @@
 #!/bin/sh
 
-# Homebrew (https://brew.sh/)
+# Homebrew (https://brew.sh/) :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 echo "Setting up Homebrew"
 if test ! $(which brew); then
   echo "Installing homebrew"
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
   echo "Homebrew allready instaled"
+  echo "Updating Homebrew"
+  brew update
 fi
 
-echo "Updating Homebrew"
-brew update
 
-# Apps installed via brew
-brew install nvm
-brew install thefuck
-brew install fx
+# Brew apps :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo "Installing apps via brew"
+brew=(
+  "nvm"
+  "thefuck"
+  "fx"
+)
+
+for app in "${brew[@]}"; do
+  echo "Installing $app"
+  brew install $app
+done
+
+# Cask apps :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo "Installing apps via brew cask"
+brew cask
+
+cask=(
+  "docker"
+  "google-chrome"
+  "spotify"
+  "slack"
+  "skype"
+  "iterm2"
+)
+
+for app in "${cask[@]}"; do
+  echo "Installing $app"
+  brew cask install $app
+done
