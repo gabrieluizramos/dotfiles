fd2750840b8b44ceaaff6032794da7c7b02695ef
Gabriel Luiz Ramos <gabrieluizramos@Gabriels-MacBook-Air.local>
2019-02-25 19:48:32 -0300

    fix zshrc link

--- config/.zsh
+++ config/.zsh
@@ -5,4 +5,4 @@ echo "Installing Oh My Zshell"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "Linkin .zshrc file"
-ln -sf ~/dotfiles/.dotfiles/.zshrc ~/.zshrcs
+ln -sf ~/dotfiles/.dotfiles/.zshrc ~/.zshrc
