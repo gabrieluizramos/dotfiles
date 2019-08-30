ed2e5ab38b91299c2cbfd975c5fc0f85e42bd7b9
Gabriel Ramos <gabriel.luiz.ramos@gmail.com>
2019-08-30 14:43:58 -0300

    chore(message): update messages and apply at setup scripts

--- config/.zsh
+++ config/.zsh
@@ -1,8 +1,9 @@
 #!/bin/sh
+source '.dotfiles/sources/.messages'
 
 # Setup Oh My Zshell (https://ohmyz.sh/) ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-echo "Installing Oh My Zshell"
+echo_installing "Installing Oh My Zshell"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "Linking .zshrc file"
+echo_default "Linking .zshrc file"
 ln -sf ~/dotfiles/.dotfiles/symlinks/.zshrc ~/.zshrc
