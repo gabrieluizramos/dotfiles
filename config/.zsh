70958eb9510f7c63c519bf289540f6dc1a1a2014
Gabriel Ramos <gabriel.luiz.ramos@gmail.com>
2019-09-02 16:25:10 -0300

    add command to set zsh as default shell

--- config/.zsh
+++ config/.zsh
@@ -5,5 +5,8 @@ source '.dotfiles/sources/.messages'
 echo_installing "Installing Oh My Zshell"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+echo_arrow "Setting Zsh as default shell"
+chsh -s /bin/zsh
+
 echo_default "Linking .zshrc file"
 ln -sf ~/dotfiles/.dotfiles/symlinks/.zshrc ~/.zshrc
