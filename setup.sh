d9ba7165ac07de2bfc353acf5f504c079d155858
gabrieluizramos <gabriel.luiz.ramos@gmail.com>
2020-12-07 09:26:24 -0300

    start ubuntu dotfiles creation

--- setup.sh
+++ setup.sh
@@ -1,8 +1,26 @@
-#!/bin/sh
+#!/bin/bash
+source '.dotfiles/sources/.messages'
 
-# Start all scripts at ./config folder
-files=$(find ./config -type f)
+# Start all scripts at ./config folder based on system
+system=$(uname);
 
-for setup in $files; do
-    sh $setup
+echo_default "Checking OS name";
+if [[ "$system" == "Darwin" ]]; then
+  echo_default "Installing MacOS dotfiles"
+  files=$(find ./config/macos -type f)
+elif [[ "$system" == "Linux" ]]; then
+  echo_default "Installing Ubuntu dotfiles"
+  files=$(find ./config/ubuntu -type f)
+fi
+
+commons=$(find ./config/common -type f)
+
+echo_default "Installing specific packages";
+for file in $files; do
+  sh $file
+done
+
+echo_default "Installing commons packages";
+for common in $commons; do
+  sh $common
 done
